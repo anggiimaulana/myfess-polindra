3f9a2a89b137a597292fb3d93c595de8ea69bcd0
Anggi Maulana <155665777+anggiimaulana@users.noreply.github.com>
2024-10-03 13:22:25 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "myfess");
+    $conn = mysqli_connect("localhost", "root", "polindra", "myfess");
     if (!$conn) {
         echo "Database connected" . mysqli_connect_error();
     }
